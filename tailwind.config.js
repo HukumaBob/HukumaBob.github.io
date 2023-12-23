47b2b09523e7b55bbc37d82803c60ccbd4d227fe
Nikita <ldc.endoscopy@gmail.com>
2023-12-23 18:51:13 +0400

    All ok

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+    theme: {
+        extend: {
+            fontFamily: {
+                roboto: ["Roboto"],
+                montserrat: ["Montserrat"],
+                lato: ["Lato"],
+                garamond: ["Garamond"]
+            },
+        },
+    },
+};
